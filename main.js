138643e3f1a8b52103a878925c7d07416c1d520c
Meowmeowc <lalachka26@mail.ru>
2023-01-19 17:33:44 +0500

    Second

---
+++ main.js
@@ -0,0 +1,54 @@
+// console.log("10")
+// console.log('Добро ' + 'Пожаловать ' + 'на курс ' + 'Дмитрий')
+// alert('19.01.2023')
+
+// 1 тип данных числовой(Number)
+console.log(Infinity)
+console.log(1 / 0)
+
+console.log(NaN)
+console.log('stroka' * 12)
+
+// 2 Тип данных Bigint
+let a = 999999999999999999999n
+console.log(a)
+
+// 3 Тип строка (string)
+let str1 = 'JOJO';
+let str2 = "JOJO2 ${str1}";
+let str3 = `ANIME ${str1}`;
+
+console.log(str1)
+console.log(str2)
+console.log(str3)
+
+// 4 тип данных Логический типа данных (BOOLean)
+let DotCheck = true
+let DotUnCheck = false
+let a = 4 > 1
+console.log(true == 0)
+
+// 5 тип данных NULL
+let nuller = null
+console.log(nuller)
+
+// 6 тип данных undifined
+
+let test;
+console.log(test)
+
+
+// 7 Тип данных объекты
+
+
+// 8 оператор typeof
+console.log(typeof undefined)
+console.log(typeof 0)
+console.log(typeof 1100n)
+console.log(typeof 'cat')
+console.log(typeof false)
+console.log(typeof Symbol('id'))
+console.log(typeof null)
+
+document.getElementById('one').innerHTML = 'Hello world'
+
